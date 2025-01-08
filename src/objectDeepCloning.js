749f3e62272998c0f55e24140dd355d4e97c7b96
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2025-01-08 17:23:07 +0530

    completed obejct cloning adding some more testcases

--- src/objectDeepCloning.js
+++ src/objectDeepCloning.js
@@ -1,3 +1,14 @@
+export const isObject = (candidate) => typeof candidate === "object";
+
 export const deepClone = (object) => {
-  return { ...object };
+  const clonedObject = {};
+
+  for (const key in object) {
+    const clonedItem = isObject(object[key])
+      ? deepClone(object[key])
+      : object[key];
+    clonedObject[key] = clonedItem;
+  }
+
+  return clonedObject;
 };
--- test/objectDeepCloning_test.js
+++ test/objectDeepCloning_test.js
@@ -8,3 +8,7 @@ Deno.test("testing with empty object", () => {
 Deno.test("testing with no nesting of objects", () => {
   assertEquals(deepClone({ a: 1 }), { a: 1 });
 });
+
+Deno.test("testing with nested objects", () => {
+  assertEquals(deepClone({ a: { b: 1 } }), { a: { b: 1 } });
+});
